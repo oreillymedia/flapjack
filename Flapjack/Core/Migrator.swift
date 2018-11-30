1ceded29f053774dbb324524ae1fbceae1c242e1
Ben Kreeger <ben@kree.gr>
2018-11-30 17:19:51 -0600

    Add migrator code, beginnings of unit tests

---
+++ Flapjack/Core/Migrator.swift
@@ -0,0 +1,29 @@
+//
+//  Migrator.swift
+//  Flapjack
+//
+//  Created by Ben Kreeger on 11/30/18.
+//  Copyright © 2018 O'Reilly Media, Inc. All rights reserved.
+//
+
+import Foundation
+
+public protocol Migrator {
+    /// If `true`, migrations do not need to be performed, and calling migrate() is a no-op.
+    var storeIsUpToDate: Bool { get }
+
+    /**
+     Performs the migration by setting up temp directories and iterating over all model versions. If an error occurs,
+     it will be thrown as a MigratorError.
+
+     - returns: `true` if a migration was performed, `false` if one was not needed.
+     */
+    func migrate() throws -> Bool
+}
+
+
+public enum MigratorError: Error {
+    case diskPreparationError
+    case proceduralError(Error)
+    case cleanupError(Error)
+}
---
+++ Tests/CoreData/CoreDataMigratorTests.swift
@@ -0,0 +1,20 @@
+//
+//  CoreDataMigratorTests.swift
+//  Tests
+//
+//  Created by Ben Kreeger on 11/30/18.
+//  Copyright © 2018 O'Reilly Media, Inc. All rights reserved.
+//
+
+import Foundation
+import XCTest
+import CoreData
+
+@testable import Flapjack
+@testable import FlapjackCoreData
+
+class CoreDataMigratorTests: XCTestCase {
+    func testPreviousModelVersionsGetTestedForMigration() {
+        // let compiledModelURL = Bundle(for: type(of: self)).url(forResource: "TestMigrationModel", withExtension: "momd")!
+    }
+}
