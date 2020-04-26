f5f1045868f6618142f0f7ef1bb69b01a31beba7
Ben Kreeger <ben@kree.gr>
2020-04-26 17:08:05 -0500

    Fix some problems with SwiftPM usage

--- Package.swift
+++ Package.swift
@@ -1,10 +1,15 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Flapjack",
+    platforms: [
+        .macOS(.v10_13),
+        .iOS(.v11),
+        .tvOS(.v11)
+    ],
     products: [
         .library(name: "Flapjack", targets: ["Flapjack"]),
         // .library(name: "FlapjackCoreData", targets: ["FlapjackCoreData"]),
