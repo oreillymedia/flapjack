c74747aaa68f1885e035272cd6bb0b6aeaa941c8
Ben Kreeger <ben@kree.gr>
2020-05-03 12:18:58 -0500

    FIX: Add the ability to accept `nil` values in NSPredicate+Extensions

--- Tests/Core/Extensions/Dictionary+ExtensionsTests.swift
+++ Tests/Core/Extensions/Dictionary+ExtensionsTests.swift
@@ -14,10 +14,13 @@ import XCTest
 class DictionaryExtensionsTests: XCTestCase {
     func testCacheKeyGeneration() {
         // Sorts keys and then spits out a key
-        let source: [String: Any] = ["this": "should", "produce": true, "aCacheKey": 42]
+        let source: [String: Any?] = ["this": "should", "produce": true, "aCacheKey": 42]
         XCTAssertEqual(source.cacheKey, "aCacheKey.42-produce.true-this.should")
 
-        let empty = [String: Any]()
+        let empty = [String: Any?]()
         XCTAssertEqual(empty.cacheKey, "")
+
+        let withNil: [String: Any?] = ["this": "should", "appear": nil]
+        XCTAssertEqual(withNil.cacheKey, "appear.nil-this.should")
     }
 }
