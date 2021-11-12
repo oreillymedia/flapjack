cb3d67501da6964c218acb0c8f35aa8f03f3c479
lj-dickey <ldickey@oreilly.com>
2021-11-12 14:31:59 -0500

    Update Package.swift to allow for Combine

--- Package.swift
+++ Package.swift
@@ -6,8 +6,8 @@ import PackageDescription
 let package = Package(
     name: "Flapjack",
     platforms: [
-        .macOS(.v10_14),
-        .iOS(.v12),
+        .macOS(.v10_15),
+        .iOS(.v13),
         .tvOS(.v12)
     ],
     products: [
